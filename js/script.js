26ede44ca48296870068e09437a8717044a1667e
Alexander K <sashakinzalov_2002@mail.ru>
2023-08-28 18:36:36 +0300

    full edit course block

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 // FORM SWITCH
-const studentButton = document.getElementById("contactFormStudentButton");
-const parentButton = document.getElementById("contactFormParentButton");
-const studentName = document.getElementById("studentName");
-const studentPhoneNumber = document.getElementById("studentPhoneNumber");
+// const studentButton = document.getElementById("contactFormStudentButton");
+// const parentButton = document.getElementById("contactFormParentButton");
+// const studentName = document.getElementById("studentName");
+// const studentPhoneNumber = document.getElementById("studentPhoneNumber");
 
-parentButton.addEventListener("change", () => {
-  if (this.checked) {
-    studentName.style.display = "inline";
-    studentPhoneNumber.style.display = "inline";
-  } else {
-    studentName.style.display = "none";
-    studentPhoneNumber.style.display = "none";
-  }
-});
+// parentButton.addEventListener("change", () => {
+//   if (this.checked) {
+//     studentName.style.display = "inline";
+//     studentPhoneNumber.style.display = "inline";
+//   } else {
+//     studentName.style.display = "none";
+//     studentPhoneNumber.style.display = "none";
+//   }
+// });
 
-studentButton.addEventListener("change", () => {
-  if (this.checked) {
-    studentName.style.display = "none";
-    studentPhoneNumber.style.display = "none";
-  } else {
-    studentName.style.display = "inline";
-    studentPhoneNumber.style.display = "inline";
-  }
-});
+// studentButton.addEventListener("change", () => {
+//   if (this.checked) {
+//     studentName.style.display = "none";
+//     studentPhoneNumber.style.display = "none";
+//   } else {
+//     studentName.style.display = "inline";
+//     studentPhoneNumber.style.display = "inline";
+//   }
+// });
 
-let coll = document.getElementsByClassName("courses__item");
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
+// let coll = document.getElementsByClassName("courses__item");
+// for (let i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//     let content = this.nextElementSibling;
 
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-      // content.style.border = "#e0caf0 0px solid";
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-      // content.style.border = "#e0caf0 4px solid";
-    }
-  });
-}
+//     if (content.style.maxHeight) {
+//       content.style.maxHeight = null;
+
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+
+//     }
+//   });
+// }
